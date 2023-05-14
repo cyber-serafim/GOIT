8888d3ac010ab75157e8fadc1970c44649272ce9
Anton Padura <78739723+cyber-serafim@users.noreply.github.com>
2023-05-14 22:55:34 +0300

    Add files via upload

---
+++ 7_ModuleHW/clean_folder/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_namespace_packages
+
+setup (
+    name='clean_folder',
+    version='0.1',
+    description='Script for sorted files',
+    url='https://github.com/cyber-serafim/GOIT/tree/main/7_ModuleHW',
+    author='Anton Padura',
+    license= No License,
+    packages=find_namespace_packages(),
+    include_package_data=True,
+    entry_points={'console_scripts':['clean-folder = clean_folder.clean:clean_f']})
